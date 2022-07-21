f26db9eeca335a13bc9dbd7d81ee8db9f8a08cd7
khaledghanem1 <khaledghanim96@gmail.com>
2022-07-20 21:53:27 -0600

    Added Prompts

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 const inquirer = require("inquirer");
+
+inquirer
+    .prompt([
+        {
+            type:"input",
+            message: "Whats the Managers Name?",
+            name: "manager",
+        },
+        {
+            type:"input",
+            message: "Whats the Managers employee ID?",
+            name: "managerID",
+        },
+        {
+            type:"input",
+            message: "Whats the Managers email address?",
+            name: "managerEmail",
+        },
+        {
+            type:"input",
+            message: "Whats the Managers office number?",
+            name: "managerNumber",
+        },
+        {
+            type:"input",
+            message: "Whats the Managers office number?",
+            name: "managerNumber",
+        },
+        {
+            type:"input",
+            message: "Whats the Managers office number?",
+            name: "managerNumber",
+        },
+        {
+            type:"input",
+            message: "Whats the Engineers name ?",
+            name: "engineerName",
+        },
+        {
+            type:"input",
+            message: "Whats the Engineers ID ?",
+            name: "engineerId",
+        },
+        {
+            type:"input",
+            message: "Whats the Engineers email?",
+            name: "engineerEmail",
+        },
+        {
+            type:"input",
+            message: "Whats the Engineers Github?",
+            name: "engineerGithub",
+        },
+    ])
