4c4cf39284208289c6dae7d72e371bc587666a2d
khaledghanem1 <khaledghanim96@gmail.com>
2022-07-20 23:26:36 -0600

    Made questions loop based on choices

--- index.js
+++ index.js
@@ -1,51 +1,97 @@
 const inquirer = require("inquirer");
 
-inquirer
+const managerQuestion = () => {
+    inquirer
+        .prompt([
+            {
+                type:"input",
+                message: "Whats the Manager's Name?",
+                name: "manager",
+            },
+            {
+                type:"input",
+                message: "Whats the Manager's employee ID?",
+                name: "managerID",
+            },
+            {
+                type:"input",
+                message: "Whats the Manager's email address?",
+                name: "managerEmail",
+            },
+            {
+                type: "list",
+                message: "Choose whether you would like to add an engineer/intern, or finish building the team",
+                name: "list",
+                choices: [
+                    "Engineer",
+                    "Intern",
+                    "Finish Building"
+                ],
+            }
+        ])
+        .then((response) => { 
+            console.log(response) 
+            if (response.list === "Engineer") {
+                getEngineer()
+            } else if (response.list === "Intern") {
+                getIntern()
+            } else {
+                console.log("error");
+            }
+        });
+}
+managerQuestion()
+
+const getEngineer = () => {
+    console.log("engineer selected")
+    inquirer
     .prompt([
         {
             type:"input",
-            message: "Whats the Manager's Name?",
-            name: "manager",
-        },
-        {
-            type:"input",
-            message: "Whats the Manager's employee ID?",
-            name: "managerID",
-        },
-        {
-            type:"input",
-            message: "Whats the Manager's email address?",
-            name: "managerEmail",
-        },
-        {
-            type: "list",
-            message: "Choose whether you would like to add an engineer or an intern",
-            name: "list",
-            choices: [
-                "Engineer",
-                "Intern",
-            ],
-        },  
-        {
-        type:"input",
-        message: "Whats the Engineer's name ?",
-        name: "engineerName",
-        },
-        {
-            type:"input",
-            message: "Whats the Engineer's ID ?",
-            name: "engineerId",
-        },
-        {
-            type:"input",
-            message: "Whats the Engineer's email?",
-            name: "engineerEmail",
-        },
-        {
-            type:"input",
-            message: "Whats the Engineer's Github?",
-            name: "engineerGithub",
-        },
+            message: "Whats the Engineer's name ?",
+            name: "engineerName",
+            },
+            {
+                type:"input",
+                message: "Whats the Engineer's ID ?",
+                name: "engineerId",
+            },
+            {
+                type:"input",
+                message: "Whats the Engineer's email?",
+                name: "engineerEmail",
+            },
+            {
+                type:"input",
+                message: "Whats the Engineer's Github?",
+                name: "engineerGithub",
+            },
+            {
+                type: "list",
+                message: "Choose whether you would like to add an engineer/intern, or finish building the team",
+                name: "list",
+                choices: [
+                    "Engineer",
+                    "Intern",
+                    "Finish Building"
+                ],
+            }
+    ])
+    .then((response) => {
+        if (response.list === "Engineer") {
+            getEngineer()
+        } else if (response.list === "Intern") {
+            getIntern()
+        } else {
+            console.log("error");
+        }
+    })
+}
+
+const getIntern = () => {
+    console.log("Intern selected")
+    inquirer
+    .prompt([
         {
             type:"input",
             message: "Whats the Intern's Name?",
@@ -54,33 +100,35 @@ inquirer
         {
             type:"input",
             message: "Whats the Intern's ID?",
-            name: "engineerId",
+            name: "internId",
         },
         {
             type:"input",
             message: "Whats the Intern's email?",
-            name: "engineerEmail",
+            name: "internEmail",
         },
         {
             type:"input",
             message: "Where did the Intern go to school?",
-            name: "engineerSchool",
+            name: "internSchool",
         },
+        {
+            type: "list",
+            message: "Choose whether you would like to add an engineer/intern, or finish building the team",
+            name: "list",
+            choices: [
+                "Engineer",
+                "Intern",
+            ],
+        }
     ])
-    .then((response) => { 
-        console.log(response) 
-        if (response.list = "Engineer") {
+    .then((response) => {
+        if (response.list === "Engineer") {
             getEngineer()
-        }
-        if (response.list = "Engineer") {
+        } else if (response.list === "Intern") {
             getIntern()
+        } else {
+            console.log("error");
         }
-    });
-
-    function getEngineer(){
-        console.log("engineer selected")
-    }
-
-    function getIntern(){
-        console.log("Intern selected")
-    }
+    })
+}
