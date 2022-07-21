aa33d42345add883acd293a41095d80b8a73354c
khaledghanem1 <khaledghanim96@gmail.com>
2022-07-20 22:50:02 -0600

    Adding more to prompt

--- index.js
+++ index.js
@@ -22,12 +22,65 @@ inquirer
             message: "Choose whether you would like to add an engineer or an intern",
             name: "list",
             choices: [
-              "Engineer",
-              "Intern",
+                "Engineer",
+                "Intern",
             ],
-          },  
+        },  
+        {
+        type:"input",
+        message: "Whats the Engineer's name ?",
+        name: "engineerName",
+        },
+        {
+            type:"input",
+            message: "Whats the Engineer's ID ?",
+            name: "engineerId",
+        },
+        {
+            type:"input",
+            message: "Whats the Engineer's email?",
+            name: "engineerEmail",
+        },
+        {
+            type:"input",
+            message: "Whats the Engineer's Github?",
+            name: "engineerGithub",
+        },
+        {
+            type:"input",
+            message: "Whats the Intern's Name?",
+            name: "internName",
+        },
+        {
+            type:"input",
+            message: "Whats the Intern's ID?",
+            name: "engineerId",
+        },
+        {
+            type:"input",
+            message: "Whats the Intern's email?",
+            name: "engineerEmail",
+        },
+        {
+            type:"input",
+            message: "Where did the Intern go to school?",
+            name: "engineerSchool",
+        },
     ])
     .then((response) => { 
         console.log(response) 
-        if (response.choices = "Engineer")
+        if (response.list = "Engineer") {
+            getEngineer()
+        }
+        if (response.list = "Engineer") {
+            getIntern()
+        }
     });
+
+    function getEngineer(){
+        console.log("engineer selected")
+    }
+
+    function getIntern(){
+        console.log("Intern selected")
+    }
